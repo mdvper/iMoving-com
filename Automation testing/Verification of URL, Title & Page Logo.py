2ee9ad5c44610318f2b75a2b0e86329e1e5a49b8
mdvper <mdvpqa@gmail.com>
2022-10-01 11:23:31 -0400

    Here you can see simple verification process of URL, Title and Logo.

---
+++ Automation testing/Verification of URL, Title & Page Logo.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+url_imoving = "https://www.imoving.com/"
+driver.get(url_imoving)
+driver.maximize_window()
+
+# URL verification
+current_url = driver.current_url
+if current_url == "https://www.imoving.com/":
+    print("Currebt URL is OK: ", current_url)
+
+# Title verification
+imoving_expectedtitle = "iMoving - Compare Moving Companies Prices and Book Online"
+current_title = driver.title
+
+if imoving_expectedtitle == current_title:
+    print ("Current Title is OK: ", imoving_expectedtitle)
+else:
+    print ("Current Title is out of scope: ", current_title)
+
+# Page Logo verification
+imoving_logo = driver.find_element(By.XPATH, '//*[@class="navbar-brand"]')
+
+if imoving_logo:
+    print("Current Logo is OK", driver.get_screenshot_as_file('imoving_cuurent_logo.png'))
+else:
+    print("iMoving Logo is not presented, or replaced with another one")
+
+driver.quit()
